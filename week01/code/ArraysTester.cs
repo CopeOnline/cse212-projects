f1ebb25dcf0332a9cf148f192f07343d1d99ee86
CopeOnline <Cop21004@byui.edu>
2024-06-11 22:53:56 -0600

    Part 1 arrays finshed

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -1,3 +1,6 @@
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
 public static class ArraysTester {
     /// <summary>
     /// Entry point for the tests
@@ -33,13 +36,25 @@ public static class ArraysTester {
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
-    {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+    {   
+        // create an array to store the results array
+        // Using a For loop to run the for loop, 'length' number of times. 
+        // int i sets the loop start point at zero;
+        // i is compared to the 'length' to make sure the current value of i is less than 'length';
+        // the value of i is incremented by i each time the loop is ran.
+        // variable 'multipler' is created and set to 1
+        // the quoietnet of 'number' and 'multiplier' is stored in results array at index of i
+        // 'multiplier' is incremeted by one.
+        // returns the array of results.
 
-        return new double[0]; // replace this return statement with your own
+        double[] results = new double[length];
+        var multiplier = 1;
+        for (double i = 0; i < length; i++)
+            {   
+                results[(int)i] = number * multiplier;
+                multiplier++;
+            }
+            return results; // replace this return statement with your own
     }
     
     /// <summary>
