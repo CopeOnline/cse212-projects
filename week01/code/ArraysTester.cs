46b40bf532e5ea79ece9d0bbed2e097dbebcd8eb
CopeOnline <Cop21004@byui.edu>
2024-06-15 11:46:26 -0700

    Finished problem 2

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -62,11 +62,31 @@ public static class ArraysTester {
     /// Because a list is dynamic, this function will modify the existing <c>data</c> list rather than returning a new list.
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
-    {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+    {   
+        // count the length of the original 'data' array 
+        // create a new array to hold the moved items
+        // in a for loop copy the elements from the end of the original array to the beginning of the new array by the 'amount'
+        // in a for loop copy the rest of the original array from the beginning of the 'data' array to the new array
+        // in a for loop copy the new array back into the 'data' array in the new order
 
+        var length = data.Count();
+
+        int [] movedRight = new int[length];
+        
+        for (int i = 0; i < amount; i++)
+            {
+                movedRight[i] = data[length - amount + i];
+            }
+        
+        for (int i = 0; i < length - amount; i++)
+            {
+                movedRight[amount + i] = data[i];
+            }
+
+       for (int i = 0; i < length; i++)
+            {
+                data[i] = movedRight[i];
+            }
+ 
     }
 }
